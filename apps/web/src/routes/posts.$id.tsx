c6eb9734079714d8807a7fb26d9a825a42759e49
Rafael Correa <rafael.correa@yamiassu.com.br>
2026-03-12 11:12:56 -0300

    feat: enhance post detail route to include related posts and improve layout

--- apps/web/src/routes/posts.$id.tsx
+++ apps/web/src/routes/posts.$id.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
 
 import { postsShow } from "@repo/domains/posts/functions";
+import { usePostsByUser } from "@repo/domains/posts/hooks";
+import { PostRow } from "@repo/domains/posts/ui/post-row";
 import { usersShow } from "@repo/domains/users/functions";
 import { UserAvatar } from "@repo/domains/users/ui/user-avatar";
 
@@ -20,16 +22,29 @@ export const Route = createFileRoute("/posts/$id")({
 
 function RouteComponent() {
 	const { post, user } = Route.useLoaderData();
+	const { data } = usePostsByUser(user.id, { limit: 5 });
 
 	return (
-		<main className="mx-auto container grow mt-20">
-			<h1 className="text-3xl font-bold">{post.title}</h1>
+		<div className="flex flex-col mt-20 md:flex-row grow mx-auto container gap-2">
+			<main className="grow">
+				<h1 className="text-3xl font-bold">{post.title}</h1>
 
-			<div className="flex my-4">
-				<UserAvatar data={user} displayName />
-			</div>
+				<div className="flex my-4">
+					<UserAvatar data={user} displayName />
+				</div>
 
-			<p>{post.body}</p>
-		</main>
+				<p>{post.body}</p>
+			</main>
+
+			<aside className="w-1/4">
+				<p className="font-bold mb-4">Related</p>
+
+				<div className="flex flex-col gap-2">
+					{data?.[0].map((post) => (
+						<PostRow key={post.id} data={post} />
+					))}
+				</div>
+			</aside>
+		</div>
 	);
 }
