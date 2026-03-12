ee6ff0a41c44a9063207a448ac73fb1002c3dc2b
Rafael Correa <rafael.correa@yamiassu.com.br>
2026-03-12 11:10:51 -0300

    feat: enhance user detail route to display posts by user and improve post row styling

--- apps/web/src/routes/users.$id.tsx
+++ apps/web/src/routes/users.$id.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
 import { MailIcon, MapIcon } from "lucide-react";
 
+import { usePostsByUser } from "@repo/domains/posts/hooks";
+import { PostRow } from "@repo/domains/posts/ui/post-row";
 import { usersShow } from "@repo/domains/users/functions";
 import { UserAvatar } from "@repo/domains/users/ui/user-avatar";
 
@@ -18,20 +20,33 @@ export const Route = createFileRoute("/users/$id")({
 
 function RouteComponent() {
 	const { user } = Route.useLoaderData();
+	const { data } = usePostsByUser(user.id, { limit: 5 });
 
 	return (
-		<main className="mx-auto container grow mt-20">
-			<h1 className="text-3xl font-bold">
-				<UserAvatar data={user} displayName />
-			</h1>
+		<div className="flex flex-col mt-20 md:flex-row grow mx-auto container">
+			<main className="grow">
+				<h1 className="text-3xl font-bold">
+					<UserAvatar data={user} displayName />
+				</h1>
 
-			<div className="flex my-4 gap-2 items-center">
-				<MailIcon size={16} /> {user.email}
-			</div>
+				<div className="flex my-4 gap-2 items-center">
+					<MailIcon size={16} /> {user.email}
+				</div>
 
-			<div className="flex gap-2 items-center">
-				<MapIcon size={16} /> {user.address}
-			</div>
-		</main>
+				<div className="flex gap-2 items-center">
+					<MapIcon size={16} /> {user.address}
+				</div>
+			</main>
+
+			<aside className="w-1/4">
+				<p className="font-bold mb-4">Posts by user</p>
+
+				<div className="flex flex-col gap-2">
+					{data?.[0].map((post) => (
+						<PostRow key={post.id} data={post} />
+					))}
+				</div>
+			</aside>
+		</div>
 	);
 }
--- packages/domains/src/domains/posts/ui/post-row.tsx
+++ packages/domains/src/domains/posts/ui/post-row.tsx
@@ -10,7 +10,7 @@ export function PostRow({ data }: { data: PostSchema }) {
 			className="border shadow rounded-lg p-4 w-full flex gap-2 flex-col"
 		>
 			<div className="text-xs text-muted-foreground">#{data.id}</div>
-			<div className="font-bold">{data.title}</div>
+			<div className="font-bold h-6 overflow-hidden text-ellipsis">{data.title}</div>
 		</Link>
 	);
 }
