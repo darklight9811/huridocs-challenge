8e94971a8770aaccc5613ff3b3c0bc4d9e40baa2
Rafael Correa <rafael.correa@yamiassu.com.br>
2026-03-13 11:11:31 -0300

    feat: refactor posts index to integrate pagination and sorting components

--- apps/web/src/routes/posts.index.tsx
+++ apps/web/src/routes/posts.index.tsx
@@ -1,6 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Pagination } from "@repo/ds/ui/pagination";
+
+import { env } from "@repo/domains/app/env";
+import { usePagination } from "@repo/domains/app/hooks";
 import { paginationSchema } from "@repo/domains/app/schema";
+import { PaginationLimitSelect } from "@repo/domains/app/ui/pagination-limit-select";
+import { PaginationSort } from "@repo/domains/app/ui/pagination-sort";
 import { postsIndex } from "@repo/domains/posts/functions";
 import { PostRow } from "@repo/domains/posts/ui/post-row";
 
@@ -17,19 +23,33 @@ export const Route = createFileRoute("/posts/")({
 });
 
 function App() {
-	const [data, pagination] = Route.useLoaderData();
+	const [data, metadata] = Route.useLoaderData();
+	const [pagination, setPagination] = usePagination();
 
 	return (
 		<main className="flex flex-col gap-4 container mx-auto mt-20 grow">
 			<h1 className="font-bold text-3xl">Posts</h1>
 
-			<div>
-				<span>total: {pagination.total}</span>
+			<div className="flex justify-between items-center">
+				<span>total: {data.length}</span>
+
+				<div className="flex gap-2">
+					<PaginationSort value={pagination.sort} onChange={(value) => setPagination({ sort: value })} />
+
+					<PaginationLimitSelect
+						value={pagination.limit || env.pagination.defaultLimit}
+						onChange={(value) => setPagination({ limit: value })}
+					/>
+				</div>
 			</div>
 
 			{data.map((post) => (
 				<PostRow data={post} key={post.id} />
 			))}
+
+			<div>
+				<Pagination {...metadata} />
+			</div>
 		</main>
-	)
+	);
 }
